119649b3e55ba295f56c0501bb3bb610b2b63ddd
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-10 21:50:17 +0100

    [Feature] Adjust form position on small screen

--- src/dom-manipulate.js
+++ src/dom-manipulate.js
@@ -9,7 +9,7 @@ const toggleForms = () => {
     left: '0',
     opacity: '1'
   });
-  $(':root').css('--currentColor', '#007bff')
+  document.querySelector(':root').style.setProperty('--currentColor', '#007bff')
 };
 
 $('.step-but').click(() => {
@@ -19,14 +19,13 @@ $('.step-but').click(() => {
     avgDailyIncomeInUSD: $('input[data-avgDailyIncomeInUSD]'),
     avgDailyIncomePopulation: $('input[data-avgDailyIncomePopulation]')
   };
-
   let chkRgnVals =
     !!rgnVals.name.text() &&
     !!rgnVals.avgAge.val() &&
     !!rgnVals.avgDailyIncomeInUSD.val() &&
     !!rgnVals.avgDailyIncomePopulation.val();
   if (chkRgnVals) {
-    rgnVals.name.data('name', rgnVals.name.text());
+    $('#name').data('name', rgnVals.name.text());
     rgnVals.avgDailyIncomeInUSD.data('avgDailyIncomeInUSD', rgnVals.avgDailyIncomeInUSD.val());
     rgnVals.avgDailyIncomePopulation.data('avgDailyIncomePopulation', rgnVals.avgDailyIncomePopulation.val());
     rgnVals.avgAge.data('avgAge', rgnVals.avgAge.val());
@@ -36,6 +35,49 @@ $('.step-but').click(() => {
   warnElem.text('Please fill all fields.');
   warnElem.css('color', 'red');
 });
+
+const runEstimator = (input) => {
+  const estimateOutput = covid19ImpactEstimator(input);
+  const { data, impact, severeImpact } = estimateOutput;
+  const { region, periodType, timeToElapse, reportedCases, population, totalHospitalBeds } =  data;
+  const { currentlyInfected, infectionsByRequestedtime, severeCasesByRequestedTime, hospitalBedsByRequestedTime, casesForICUByRequestedTime, casesForVentilatorsByRequestedTime, dollarsInFlight } = impact;
+  const { currentlyInfected: scurrentlyInfected, infectionsByRequestedtime: sinfectionsByRequestedtime, severeCasesByRequestedTime: ssevereCasesByRequestedTime , hospitalBedsByRequestedTime: shospitalBedsByRequestedTime, casesForICUByRequestedTime: scasesForICUByRequestedTime, casesForVentilatorsByRequestedTime: scasesForVentilatorsByRequestedTime, dollarsInFlight: sdollarsInFlight } = severeImpact;
+  const { name, avgAge, avgDailyIncomeInUSD, avgDailyIncomePopulation } = region;
+  
+  $('.reg').text(name);
+  $('.avgAge').text(avgAge);
+  $('.avgDailyIncomeInUSD').text(avgDailyIncomeInUSD);
+  $('.avgDailyIncomePopulation').text(avgDailyIncomePopulation);
+  $('.periodType').text(periodType);
+  $('.timeToElapse').text(timeToElapse);
+  $('.reportedCases').text(reportedCases);
+  $('.population').text(population);
+  $('.totalHospitalBeds').text(totalHospitalBeds);
+  $('.impact-cont .currentlyInfected').text(currentlyInfected);
+  $('.impact-cont .infectionsByRequestedtime').text(infectionsByRequestedtime);
+  $('.impact-cont .severeCasesByRequestedTime').text(severeCasesByRequestedTime);
+  $('.impact-cont .hospitalBedsByRequestedTime').text(hospitalBedsByRequestedTime);
+  $('.impact-cont .casesForICUByRequestedTime').text(casesForICUByRequestedTime);
+  $('.impact-cont .casesForVentilatorsByRequestedTime').text(casesForVentilatorsByRequestedTime);
+  $('.impact-cont .dollarsInFlight').text(dollarsInFlight);
+  $('.severe-impact-cont .currentlyInfected').text(scurrentlyInfected);
+  $('.severe-impact-cont .infectionsByRequestedtime').text(sinfectionsByRequestedtime);
+  $('.severe-impact-cont .severeCasesByRequestedTime').text(ssevereCasesByRequestedTime);
+  $('.severe-impact-cont .hospitalBedsByRequestedTime').text(shospitalBedsByRequestedTime);
+  $('.severe-impact-cont .casesForICUByRequestedTime').text(scasesForICUByRequestedTime);
+  $('.severe-impact-cont .casesForVentilatorsByRequestedTime').text(scasesForVentilatorsByRequestedTime);
+  $('.severe-impact-cont .dollarsInFlight').text(sdollarsInFlight);
+
+  $('.form-container section').css('width', '100%');
+  $('aside').css('width', '100%');
+
+  $('.steps-shift').hide(1000);
+  $('form').hide(1000);
+  $('aside h1').hide(1000);
+  $('aside h4').hide(1000);
+  $('.results').show( "explode", {pieces: 9 }, 2000 )
+  $('.data-main-cont').show( "explode", {pieces: 9 }, 2000 )
+}
 
 $('form').submit((e) => {
   e.preventDefault();
@@ -50,12 +92,13 @@ $('form').submit((e) => {
         'avgDailyIncomePopulation'
       )
     },
-    periodType: $('input[data-avgAge]').val(),
+    periodType: $('select[data-periodType] option:selected').text(),
     timeToElapse: $('input[data-timeToElapse]').val(),
     reportedCases: $('input[data-reportedCases]').val(),
     population: $('input[data-population]').val(),
     totalHospitalBeds: $('input[data-totalHospitalBeds]').val()
   };
-
-  covid19ImpactEstimator(data);
+  runEstimator(data);
 });
+
+
--- src/estimator.js
+++ src/estimator.js
@@ -78,25 +78,3 @@ const covid19ImpactEstimator = data => {
 };
 
 export default covid19ImpactEstimator;
-
-// Input
-// {
-//   region: {
-//   name: "Africa",
-//   avgAge: 19.7,
-//   avgDailyIncomeInUSD: 5,
-//   avgDailyIncomePopulation: 0.71
-//   },
-//   periodType: "days",
-//   timeToElapse: 58,
-//   reportedCases: 674,
-//   population: 66622705,
-//   totalHospitalBeds: 1380614
-//  }
-
-// Output
-// {
-//   data: {}, // the input data you got
-//   impact: {}, // your best case estimation
-//   severeImpact: {} // your severe case estimation
-//  }
