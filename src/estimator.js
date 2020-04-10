d3acd865b8b716037c18e7e2004a52d85b617cf4
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-10 20:12:17 +0100

    [Feature] Create function call on runEstimator()

--- src/estimator.js
+++ src/estimator.js
@@ -76,21 +76,7 @@ const covid19ImpactEstimator = data => {
     severeImpact
   };
 };
-let output = covid19ImpactEstimator({
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: 'days',
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-});
 
-console.log(output);
 export default covid19ImpactEstimator;
 
 // Input
